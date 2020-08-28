92df139fbe338cc80013b9da33062ec3f6220995
Sean Sundberg <seansund@us.ibm.com>
2020-08-28 12:52:35 -0500

    Adds key references in generated secret
    - Adds annotations to the generated Secret containing the Key Protect instance id and the key ids used to populate the values

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module argocd-plugin-key-protect
 go 1.14
 
 require (
+	github.com/imdario/mergo v0.3.11
 	github.com/urfave/cli/v2 v2.2.0
 	gopkg.in/yaml.v2 v2.3.0
 )
--- pkg/generate_secret/main.go
+++ pkg/generate_secret/main.go
@@ -6,26 +6,37 @@ import (
 	"argocd-plugin-key-protect/models/metadata"
 	kp "argocd-plugin-key-protect/pkg/key_protect"
 	"encoding/base64"
+	"github.com/imdario/mergo"
 )
 
 func GenerateSecret(kp kpModel.Secret) kubernetes.Secret {
 	var values map[string]string
+	var annotations map[string]string
 
 	values = make(map[string]string)
+	annotations = make(map[string]string)
+
+	addKeyProtectInstanceId(&annotations)
 
 	specValues := kp.Spec.Values
 	for _, kps := range specValues {
-		values[kps.Name] = convertValue(kps)
+		values[kps.Name] = convertValue(kps, &annotations)
 	}
 
-	return kubernetes.New(metadata.New(kp.Metadata.Name, kp.Spec.Labels, kp.Spec.Annotations), values)
+	mergo.Merge(&annotations, kp.Spec.Annotations)
+
+	return kubernetes.New(metadata.New(kp.Metadata.Name, kp.Spec.Labels, annotations), values)
 }
 
-func convertValue(kp kpModel.SecretValue) string {
+func convertValue(kp kpModel.SecretValue, annotations *map[string]string) string {
 	var result string
 
 	if kp.KeyId != "" {
-		result = lookupValueFromKeyId(kp.KeyId)
+		result = lookupValueFromKeyProtect(kp.KeyId)
+
+		a := *annotations
+
+		a["key-protect.key-id/" + kp.Name] = kp.KeyId
 	} else if kp.Value != "" {
 		result = base64.StdEncoding.EncodeToString([]byte(kp.Value))
 	} else {
@@ -35,6 +46,10 @@ func convertValue(kp kpModel.SecretValue) string {
 	return result
 }
 
-func lookupValueFromKeyId(keyId string) string {
+func lookupValueFromKeyProtect(keyId string) string {
 	return kp.GetKey(keyId)
+}
+
+func addKeyProtectInstanceId(annotations *map[string]string) {
+	kp.AddKeyProtectInstanceId(annotations)
 }
